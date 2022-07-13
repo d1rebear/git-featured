2fffcf93958d0f797cb6bc260285720ac7c3d036
xodohtro <103849368+xodohtro@users.noreply.github.com>
2022-07-13 22:49:41 +0500

    get rid of name shadowing

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """See the file "LICENSE" for the full license governing this code."""
 import typer
-from commands import checkout, clone, list, merge, pull, push, show
+from commands import checkout, clone, list as __list, merge, pull, push, show
 app = typer.Typer()
 
 app.add_typer(checkout.app, name='checkout')
 app.add_typer(clone.app, name='clone')
-app.add_typer(list.app, name='list')
+app.add_typer(__list.app, name='list')
 app.add_typer(merge.app, name='merge')
 app.add_typer(pull.app, name='pull')
 app.add_typer(push.app, name='push')
